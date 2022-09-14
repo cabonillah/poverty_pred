bf7e60a8404a9c776e127f9e63e98bbacb5e6d0a
Camilo Andrés Bonilla Hernández <ca_m_ilo@hotmail.com>
2022-09-13 19:07:01 -0500

    Merged data and initial filtering

--- scripts/data_merging.R
+++ scripts/data_merging.R
@@ -1,1 +1,14 @@
-############## I'M SUPPOSED TO BE DELETED #################
+library("tidyverse")
+
+df_per <- readRDS("../stores/train_personas.Rds")
+df_hog <- readRDS("../stores/train_hogares.Rds")
+
+data <- df_hog %>% left_join(df_per, by = "id")
+
+# Select those variables whose number of na's does not exceed 33% of the total number of observations
+data <- data %>%
+    select(
+        where(
+            ~ sum(is.na(.)) <= (nrow(data) * 0.33)
+        )
+    )
