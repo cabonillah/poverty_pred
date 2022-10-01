c84e07a6014f32769067e010121ff9785d3dff5d
Camilo Andrés Bonilla Hernández <ca_m_ilo@hotmail.com>
2022-09-30 20:19:39 -0500

    Created modules

---
+++ scripts/rf_clas.R
@@ -0,0 +1,34 @@
+source("../scripts/train_test_impute.R")
+source("../scripts/workflows.R")
+source("../scripts/grids.R")
+source("../scripts/tuning.R")
+
+wf <- workflows("rf", "clas")
+grid <- grids("rf", "clas")
+result <- wf %>% tuning("rf", "clas", grid, validation_split)
+result %>% collect_metrics()
+
+# Select best model
+best <- select_best(result, metric = "f_score")
+# Finalize the workflow with those parameter values
+final_wf <- wf %>% finalize_workflow(best)
+# Fit on training, predict on test, and report performance
+lf <- last_fit(final_wf, data_split)
+# Performance metric on test set
+metric <- f_score(
+    data.frame(
+        test["Pobre"],
+        lf %>% extract_workflow() %>% predict(test)
+    ),
+    Pobre,
+    .pred_class
+)$.estimate
+
+# Final report for this model
+report <- data.frame(
+    Problema = "Clas.", Modelo = "Random Forest",
+    Penalidad = "0.000325", Mixtura = "0.3666667",
+    result %>% show_best(n = 1) %>% mutate(mean = metric)
+)
+
+saveRDS(report, file = "../stores/rf_clas.rds")
---
+++ scripts/tuning.R
@@ -0,0 +1,54 @@
+library("yardstick")
+library("tune")
+
+f_score <- function(data,
+                    truth,
+                    estimate,
+                    beta,
+                    estimator,
+                    na_rm,
+                    case_weights,
+                    ...) {
+    yardstick::f_meas(
+        data = data,
+        truth = !!rlang::enquo(truth),
+        estimate = !!rlang::enquo(estimate),
+        beta = 1.75,
+        estimator = NULL,
+        na_rm = TRUE,
+        case_weights = NULL,
+        ...
+    )
+}
+
+f_score <- new_class_metric(f_score, "maximize")
+
+tuning <- function(object, model, reg_clas, grid, resamples, ...) {
+    if (model == "lm" && reg_clas == "reg") {
+        tune <- tune::tune_grid(
+            object = object,
+            metrics = yardstick::metric_set(rmse),
+            resamples = resamples
+        )
+    }
+
+    if (model %in% c("lm", "lasso", "ridge", "elastic", "rf") && reg_clas == "clas") {
+        tune <- tune::tune_grid(
+            object = object,
+            grid = grid,
+            metrics = yardstick::metric_set(f_score),
+            resamples = resamples
+        )
+    }
+
+    if (model != "lm" && reg_clas == "reg") {
+        tune <- tune::tune_grid(
+            object = object,
+            grid = grid,
+            metrics = yardstick::metric_set(rmse),
+            resamples = resamples
+        )
+    }
+
+    tune
+}
