ece2bff475b8cd7fef93a9a00b5770f89f24eea6
rasantofimior <r.santofimio@uniandes.edu.co>
2022-10-01 08:58:40 -0500

    output rf

--- scripts/yolo.R
+++ scripts/yolo.R
@@ -2,3 +2,9 @@ source("../scripts/rf_clas.R")
 saveRDS(result, file = "../../results1.rds")
 source("../scripts/rf_reg.R")
 saveRDS(result, file = "../../results2.rds")
+
+RF_CLASS <- readRDS("../../results1.rds")
+autoplot(RF_CLASS)
+
+RF_RES <- readRDS("../../results2.rds")
+autoplot(RF_RES)
